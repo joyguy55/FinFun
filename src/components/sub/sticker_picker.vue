4f69ac2616995a905bc6d845621a8f33be71cc52
Chase Jones <chasej@IDA-WS-065-mac.local>
2017-09-25 11:43:56 -0600

    Repositioned Triangle for popover and started reworking Tabs

--- src/components/sub/sticker_picker.vue
+++ src/components/sub/sticker_picker.vue
@@ -1,22 +1,16 @@
 <template>
-    <div class="sticker_picker_margin">
-        <div class="sticker_picker_container popover_triangle">
-            <button type="button"
-                    class="close close_box"
-                    aria-label="Close"
-                    v-on:click="closeColorBox()">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            <button id="sticker_button"
-                    class="sticker_box"
-                    v-for="sticker in stickerSet"
-                    type="button"
-                    v-on:click="selectSticker({index:position, id:sticker.id})">
-                <img :src="sticker.image">
-            </button>
-            <button type="reset" id="clear_options" class="btn btn-default btn-sm">clear</button>
-        </div>
-    </div>
+<div class="sticker_picker_margin">
+  <div class="sticker_picker_container">
+    <button type="button" class="close close_box" aria-label="Close" v-on:click="closeColorBox()">
+        <span aria-hidden="true">&times;</span>
+    </button>
+    <button id="sticker_button" class="sticker_box" v-for="sticker in stickerSet" type="button" v-on:click="selectSticker({index:position, id:sticker.id})">
+        <img :src="sticker.image">
+    </button>
+    <button type="reset" id="clear_options" class="btn btn-default btn-sm">clear</button>
+    <div class="popover_triangle"></div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -28,36 +22,36 @@ import {
 
 export default {
   name: 'sticker_picker',
-  data () {
+  data() {
     return {
-        triangle_position: [ 24, 50 ],
-        currentStickers: this.stickerSet
-      }
+      triangle_position: [ 24, 50 ],
+      currentStickers: this.stickerSet
+    }
   },
-  props: ['stickerArr','position','stickerBox'],
+  props: [ 'stickerArr', 'position', 'stickerBox' ],
   computed: {
-    ...mapState({
+    ...mapState( {
       stickers: state => state.stickers,
       selectedStickers: state => state.selectedStickers
-    }),
-    stickerSet(){
-      const predicate = (start,end) => {
-        return (sticker)=> {
-          return _.inRange(sticker.id, start, end)
+    } ),
+    stickerSet() {
+      const predicate = ( start, end ) => {
+        return ( sticker ) => {
+          return _.inRange( sticker.id, start, end )
         }
       }
-      let start = this.$props.stickerArr[0]
-      let end = this.$props.stickerArr[1]
-      const stickers = _.filter(this.stickers, predicate(start,end))
+      let start = this.$props.stickerArr[ 0 ]
+      let end = this.$props.stickerArr[ 1 ]
+      const stickers = _.filter( this.stickers, predicate( start, end ) )
       return stickers
     }
   },
   methods: {
-    ...mapActions([
+    ...mapActions( [
       'selectSticker'
-    ]),
+    ] ),
     closeColorBox() {
-      this.$emit('update:stickerBox', false)
+      this.$emit( 'update:stickerBox', false )
     }
   }
 }
@@ -65,7 +59,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-  @import 'sticker_picker.scss'
+@import 'sticker_picker.scss'
 </style>
 
 // :class="[ image.isActive ? 'sticker_box active' : 'sticker_box' ]"
